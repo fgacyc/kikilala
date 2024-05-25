e32bd2faaa49a4e02b87eb1458d2f1ab631272e5
Innis <1575270674@qq.com>
2024-05-25 14:17:34 +0800

    feat: migration headcount

--- src/api/headcount.js
+++ src/api/headcount.js
@@ -1,56 +1,97 @@
 import {addDoc, deleteDoc, readAllDocs, readDoc, updateDoc,queryDoc} from "./firebase.js";
+import {withRetry} from "../tools.js";
+
+const host_url = import.meta.env.VITE_HOST_URL;
 
 // create
-export async function addHeadcount(data) {
+export async function addHeadcount1(data) {
     let docID = await addDoc("headcount", data);
     if (docID === false) return false;
     return docID;
 }
 
-// read
-export async function readHeadcount(docID) {
-    let doc = await readDoc("headcount", docID);
-    if (doc === false) return false;
-    return doc;
+export async function addHeadcount(headcountData){
+    return await withRetry(async () => {
+        const response = await fetch(`${host_url}/headcount`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(headcountData)
+        });
+        const data = await response.json();
+        console.log(data)
+        if (data.status === true) return data.data.uuid;
+        throw new Error('Failed to add headcount');
+    },2,1000);
+
 }
 
-// export async function readAttendByCGName(cg_name) {
-//     const query = ["cg_name", "==", cg_name];
-//     let doc = await queryDoc("headcount", query);
-//     if (doc === false) return false;
-//     for (let i = 0; i < doc.length; i++) {
-//         doc[i].key = String(i);
-//     }
-//     // console.log(doc)
-//     return doc;
-// }
 
 // read all
-export async function readAllHeadcounts() {
+export async function readAllHeadcounts1() {
     let docs = await readAllDocs("headcount");
     if (docs === false) return false;
     return docs;
 }
 
-// export  async  function queryAttends(date){
-//     if (!date) return false;
-//     // console.log(date)
-//     const query = ["date", "==",date]
-//     const doc = await queryDoc("headcount", query);
-//     if (doc === false) return false;
-//     return doc;
-// }
+export async function readAllHeadcounts() {
+    return await withRetry(async () => {
+        const response = await fetch(`${host_url}/headcount`, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        });
+        const data = await response.json();
+        if (data.status === true) return data.data;
+        return false;
+    },2,1000);
+
+}
+
 
 // update
-export async function updateHeadcount(docID, data) {
+export async function updateHeadcount1(docID, data) {
     let res = await updateDoc("headcount", data, docID);
     if (res === false) return false;
     return res;
 }
 
+export async function updateHeadcount(docID, headcount_data) {
+    return await withRetry(async () => {
+        const response = await fetch(`${host_url}/headcount/${docID}`, {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(headcount_data)
+        });
+        const data = await response.json();
+        if (data.status === true) return data.data;
+        throw new Error('Failed to update headcount');
+    },2,1000);
+
+}
+
 // delete
-export async function deleteHeadcount(docID) {
+export async function deleteHeadcount1(docID) {
     let res = await deleteDoc("headcount", docID);
     if (res === false) return false;
     return res;
+}
+
+export async function deleteHeadcount(docID) {
+    return await withRetry(async () => {
+        const response = await fetch(`${host_url}/headcount/${docID}`, {
+            method: 'DELETE',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        });
+        const data = await response.json();
+        if (data.status === true) return true;
+        return false;
+    },2,1000);
+
 }
